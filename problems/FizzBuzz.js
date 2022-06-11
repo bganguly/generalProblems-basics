f1050bed5a6b738009fa935091c3d1fca6a824d1
bganguly <gangulybikramjit@hotmail.com>
2022-06-11 01:18:59 -0700

    added more online assessment problems

---
+++ problems/FizzBuzz.js
@@ -0,0 +1,8 @@
+/*
+given an array of n  integers 
+print, on a new line for each int
+- fizz if divisible by 3
+- buzz if divisible by 5
+- fizzbuzz if divisible by 15
+- the integer itself id none of the above occur
+*/
---
+++ problems/create-function-with-specific-mutators.js
@@ -0,0 +1,12 @@
+/*
+given a function object, that can store strings and 
+supports an add/remove and getList operation as described below.
+add should only add new string
+remove should remove from collection
+getlist should return array of current strings, in the same order, as they 
+were inserted in
+objectInstance.add('shirt')
+objectInstance.add('pant')
+objectInstance.getList() // should return ['shirt,'pant']
+
+*/

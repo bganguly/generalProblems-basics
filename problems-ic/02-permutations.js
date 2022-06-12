f36955464f085db913f3f70a46bebda32c550573
bganguly <gangulybikramjit@hotmail.com>
2022-06-12 10:55:17 -0700

    updated permutations solution

--- problems-ic/02-permutations.js
+++ problems-ic/02-permutations.js
@@ -4,34 +4,37 @@
 
  // originally from  - https://www.interviewcake.com/question/javascript/recursive-string-permutations?course=fc1&section=dynamic-programming-recursion
 
- function getPermutations(string) {
+ const  getPermutations =  string => {
+
   // Base case
   if (string.length <= 1) {
     return new Set([string]);
+  } else {
+    const allCharsExceptLast = string.slice(0, -1);
+  
+    // Recursive call: get all possible permutations for all chars except last
+    const permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptLast);
+  
+    // Put the last char in all possible positions for each of the above permutations
+    const permutations = new Set();
+    console.log(new Date(), 'string', string, 'permutationsOfAllCharsExceptLast', permutationsOfAllCharsExceptLast);
+    const lastChar = string[string.length - 1];
+    permutationsOfAllCharsExceptLast.forEach(permutationOfAllCharsExceptLast => {
+      for (let position = 0; position <= allCharsExceptLast.length; position++) {
+        const permutation =
+          permutationOfAllCharsExceptLast.slice(0, position) 
+          + lastChar 
+          + permutationOfAllCharsExceptLast.slice(position);
+        permutations.add(permutation);
+      }
+    });
+    console.log(new Date(),  'string', string,'permutationsInclLastChar', permutations);
+    return permutations;
   }
 
-  const allCharsExceptLast = string.slice(0, -1);
-  // the below will also work
-  // const allCharsExceptLast = string.substring(0, string.length - 1);
-  const lastChar = string[string.length - 1];
-
-  // Recursive call: get all possible permutations for all chars except last
-  const permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptLast);
-
-  // Put the last char in all possible positions for each of the above permutations
-  const permutations = new Set();
-  permutationsOfAllCharsExceptLast.forEach(permutationOfAllCharsExceptLast => {
-    for (let position = 0; position <= allCharsExceptLast.length; position++) {
-      const permutation =
-        permutationOfAllCharsExceptLast.slice(0, position) 
-        + lastChar 
-        + permutationOfAllCharsExceptLast.slice(position);
-      permutations.add(permutation);
-    }
-  });
-
-  return permutations;
 }
 
-console.log(getPermutations('ca'))
+const inputStr = 'cat'
+console.log('inputStr : ',inputStr)
+console.log(getPermutations(inputStr))
  

c67a853774964fcef52b336fde6fde833fe90878
bganguly <gangulybikramjit@hotmail.com>
2022-06-11 19:05:29 -0700

    added fizzbuzz solution

--- problems/fizz-buzz.js
+++ problems/fizz-buzz.js
@@ -6,3 +6,19 @@ print, on a new line for each int <= n,
 - fizzbuzz if divisible by 15
 - the integer itself if none of the above occur
 */
+
+const printFizzBuzz = n => {
+  for (let i=1; i<=n; i++) {
+    if ( i % 15 === 0) {
+      console.log('FizzBuzz')
+    } else if ( i % 3 === 0) {
+      console.log('Fizz')
+    } else if ( i % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+printFizzBuzz(31)

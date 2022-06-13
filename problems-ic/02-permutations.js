bd3426d5f95129657c51bf6d2d0f0f7d46f4842e
bganguly <gangulybikramjit@hotmail.com>
2022-06-12 17:39:23 -0700

    updated permutations solution

--- problems-ic/02-permutations.js
+++ problems-ic/02-permutations.js
@@ -10,25 +10,27 @@
   if (string.length <= 1) {
     return new Set([string]);
   } else {
-
     const allCharsExceptLast = string.slice(0, -1);
     // Recursive call: get all possible permutations for all chars except last
     const permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptLast);
   
     // Put the last char in all possible positions for each of the above permutations
-    const permutations = new Set();
-    // console.log(new Date(), 'string', string, 'permutationsOfAllCharsExceptLast', permutationsOfAllCharsExceptLast);
-    const lastChar = string[string.length - 1];
-    permutationsOfAllCharsExceptLast.forEach(eachPermutation => {
-      for (let position = 0; position <= allCharsExceptLast.length; position++) {
-        const permutation = eachPermutation.slice(0, position) + lastChar + eachPermutation.slice(position);
-        permutations.add(permutation);
-      }
-    });
-    // console.log(new Date(),  'string', string,'permutationsInclLastChar', permutations);
-    return permutations;
+    return getPermutationSet(string, permutationsOfAllCharsExceptLast, allCharsExceptLast);
   }
+}
 
+const getPermutationSet = (string, permutationsOfAllCharsExceptLast, allCharsExceptLast) => {
+  console.log(new Date(), 'string', string, 'permutationsOfAllCharsExceptLast', permutationsOfAllCharsExceptLast);
+  const permutations = new Set();
+  const lastChar = string[string.length - 1];
+  permutationsOfAllCharsExceptLast.forEach(eachPermutation => {
+    for (let position = 0; position <= allCharsExceptLast.length; position++) {
+      const permutation = eachPermutation.slice(0, position) + lastChar + eachPermutation.slice(position);
+      permutations.add(permutation);
+    }
+  });
+  console.log(new Date(),  'string', string,'permutationsInclLastChar', permutations);
+  return permutations;
 }
 
 const inputStr = 'cat'

209dcfcce54f758e580063952d39ac5f182eb74f
bganguly <gangulybikramjit@hotmail.com>
2022-07-03 17:41:17 -0700

    updated fizzbuzz solution to use a helper function

--- online-assessment-problems/fizz-buzz.js
+++ online-assessment-problems/fizz-buzz.js
@@ -9,16 +9,22 @@ print, on a new line for each int <= n,
 
 const printFizzBuzz = n => {
   for (let i=1; i<=n; i++) {
-    if ( i % 15 === 0) {
-      console.log('FizzBuzz')
-    } else if ( i % 3 === 0) {
-      console.log('Fizz')
-    } else if ( i % 5 === 0) {
-      console.log('Buzz')
-    } else {
-      console.log(i)
-    }
+    console.log(getValue(i))
   }
+}
+
+const getValue = i => {
+  let output
+  if (i % 15 === 0) {
+    output= 'FizzBuzz'
+  } else if (i % 3 === 0) {
+    output = 'Fizz'
+  } else if (i % 5 === 0) {
+    output = 'Buzz'
+  } else {
+    output = i
+  }
+  return output
 }
 
 printFizzBuzz(31)
